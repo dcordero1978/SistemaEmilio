46aa845ae5bfbd3562a7edc95c1173bab20d4129
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-08-15 16:53:11 -0600

    Avances de la pantalla Empleado

--- src/main/java/ni/gob/inss/sisinv/view/bean/backbean/catalogo/EmpleadoBackBean.java
+++ src/main/java/ni/gob/inss/sisinv/view/bean/backbean/catalogo/EmpleadoBackBean.java
@@ -1,5 +1,92 @@
 package ni.gob.inss.sisinv.view.bean.backbean.catalogo;
 
-public class EmpleadoBackBean {
+import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import ni.gob.inss.barista.view.bean.backbean.BaseBackBean;
+
+@Named
+@Scope("view")
+public class EmpleadoBackBean extends BaseBackBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String primerNombre;
+	private String segundoNombre;
+	private String primerApellido;
+	private String segundoApellido;
+	private String txtBusquedaEmpleado;
+	private Integer hfId;
+	
+	@PostConstruct
+	public void init(){
+		limpiaFormularioVista();
+		System.out.println("Finalizando inicialización de  EmpleadoBackBean....");
+	}
+	
+	public void limpiaFormularioVista(){
+		this.setPrimerNombre("");
+		this.setSegundoNombre("");
+		this.setPrimerApellido("");
+		this.setSegundoApellido("");
+		this.setTxtBusquedaEmpleado("");
+	}
+	
+	public void buscarEmpleadoByName(){
+		System.out.println("Buscar empleado");
+	}
+
+	public String getPrimerNombre() {
+		return primerNombre;
+	}
+
+	public void setPrimerNombre(String primerNombre) {
+		this.primerNombre = primerNombre;
+	}
+
+	public String getSegundoNombre() {
+		return segundoNombre;
+	}
+
+	public void setSegundoNombre(String segundoNombre) {
+		this.segundoNombre = segundoNombre;
+	}
+
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+
+	public String getTxtBusquedaEmpleado() {
+		return txtBusquedaEmpleado;
+	}
+
+	public void setTxtBusquedaEmpleado(String txtBusquedaEmpleado) {
+		this.txtBusquedaEmpleado = txtBusquedaEmpleado;
+	}
+
+	public Integer getHfId() {
+		return hfId;
+	}
+
+	public void setHfId(Integer hfId) {
+		this.hfId = hfId;
+	}
+
+	
+	
 }

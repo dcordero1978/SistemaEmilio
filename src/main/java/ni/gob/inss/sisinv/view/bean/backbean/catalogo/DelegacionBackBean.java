8b3413c50fda1cb2d4772c8e823474876470b6b1
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-08-15 13:32:48 -0600

    Acciones Agregar y Editar en pantalla delegación Se agregan los botones Agregar y Editar junto con sus metodos Sin funcionalidad.

--- src/main/java/ni/gob/inss/sisinv/view/bean/backbean/catalogo/DelegacionBackBean.java
+++ src/main/java/ni/gob/inss/sisinv/view/bean/backbean/catalogo/DelegacionBackBean.java
@@ -1,5 +1,55 @@
 package ni.gob.inss.sisinv.view.bean.backbean.catalogo;
 
-public class DelegacionBackBean {
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import ni.gob.inss.barista.view.bean.backbean.BaseBackBean;
+
+@Named
+@Scope("view")
+public class DelegacionBackBean extends BaseBackBean implements Serializable  {
+
+	private static final long serialVersionUID = 1L;
+	private String txtBusquedaDelegacionByNombre;
+	private String nombreDelegacion;
+	
+	@PostConstruct
+	public void init(){
+		
+	}
+	
+	public void buscarDelegacionByName(){
+		
+	}
+	
+	public void crearDelegacion(){
+	
+	}
+	
+	public void editarDelegacion(){
+	
+	}
+
+	public String getNombreDelegacion() {
+		return nombreDelegacion;
+	}
+
+	public void setNombreDelegacion(String nombreDelegacion) {
+		this.nombreDelegacion = nombreDelegacion;
+	}
+
+	public String getTxtBusquedaDelegacionByNombre() {
+		return txtBusquedaDelegacionByNombre;
+	}
+
+	public void setTxtBusquedaDelegacionByNombre(String txtBusquedaDelegacionByNombre) {
+		this.txtBusquedaDelegacionByNombre = txtBusquedaDelegacionByNombre;
+	}
+	
+	
 
 }

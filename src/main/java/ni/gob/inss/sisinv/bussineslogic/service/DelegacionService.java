bd6f089ca2697b65c3d9913c9c4bf82394a40024
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-08-22 11:36:45 -0600

    Pantalla Delegación  - Se agrega la capa de Servicio Se agrega la interfaz de servicio de Delegación junto con su implementación, dentro de ellas los metodos comunes pero auno no se ha desarrollado su respectiva funcionalidad.

--- src/main/java/ni/gob/inss/sisinv/bussineslogic/service/DelegacionService.java
+++ src/main/java/ni/gob/inss/sisinv/bussineslogic/service/DelegacionService.java
@@ -1,5 +1,20 @@
 package ni.gob.inss.sisinv.bussineslogic.service;
 
-public interface DelegacionService {
+import java.util.List;
 
+import ni.gob.inss.barista.businesslogic.service.BusinessException;
+import ni.gob.inss.barista.model.dao.DAOException;
+import ni.gob.inss.barista.model.dao.EntityNotFoundException;
+import ni.gob.inss.sisinv.model.entity.catalogo.Delegacion;
+
+public interface DelegacionService {
+	
+	public void agregar(Delegacion oDelegacion) throws DAOException, BusinessException;
+	
+	public void actualizar(Delegacion oDelegacion) throws DAOException, BusinessException;
+	
+	public List<Delegacion> buscar(String txtCriterio);
+	
+	public Delegacion obtener(int id) throws EntityNotFoundException;
+	
 }
--- src/main/java/ni/gob/inss/sisinv/bussineslogic/serviceImpl/DelegacionServiceImpl.java
+++ src/main/java/ni/gob/inss/sisinv/bussineslogic/serviceImpl/DelegacionServiceImpl.java
@@ -1,5 +1,50 @@
 package ni.gob.inss.sisinv.bussineslogic.serviceImpl;
 
-public class DelegacionServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ni.gob.inss.barista.businesslogic.service.BusinessException;
+import ni.gob.inss.barista.model.dao.DAOException;
+import ni.gob.inss.barista.model.dao.EntityNotFoundException;
+import ni.gob.inss.sisinv.bussineslogic.service.DelegacionService;
+import ni.gob.inss.sisinv.model.dao.DelegacionDAO;
+import ni.gob.inss.sisinv.model.entity.catalogo.Delegacion;
+
+@Service
+public class DelegacionServiceImpl implements DelegacionService{
+	
+	@Autowired
+	DelegacionDAO oDelegacionDAO;
+
+	@Transactional
+	@Override
+	public void agregar(Delegacion oDelegacion) throws DAOException, BusinessException {
+			oDelegacionDAO.saveUpper(oDelegacion);		
+	}
+
+	@Override
+	public void actualizar(Delegacion oDelegacion) throws DAOException, BusinessException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	@Override
+	public List<Delegacion> buscar(String txtCriterio) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Delegacion obtener(int id) throws EntityNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
 
 }

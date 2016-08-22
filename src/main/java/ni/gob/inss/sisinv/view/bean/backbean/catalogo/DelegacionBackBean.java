c608e10dba7de055f377af6a7fa246a15c275529
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-08-22 11:35:00 -0600

    Pantalla Delegación - Metodo Guardar 1.- Se agregan nuevos campos para el manejo de algunas propiedades en la vista:     1.1 listaDelegaciones.     1.2 delegacionSeleccionada     1.3 nombreDelegacion     1.4 hfId     1.5 pasivo     1.6 departamentoId Esto con sus respectivos getters y setters 2.- Se agrega funcionalidad al metodo guardar, pero aun no es funcional.

--- src/main/java/ni/gob/inss/sisinv/view/bean/backbean/catalogo/DelegacionBackBean.java
+++ src/main/java/ni/gob/inss/sisinv/view/bean/backbean/catalogo/DelegacionBackBean.java
@@ -15,18 +15,28 @@ import ni.gob.inss.barista.model.entity.catalogo.Catalogo;
 import ni.gob.inss.barista.model.entity.catalogo.TiposCatalogo;
 import ni.gob.inss.barista.view.bean.backbean.BaseBackBean;
 import ni.gob.inss.barista.view.utils.web.MessagesResults;
+import ni.gob.inss.sisinv.bussineslogic.service.DelegacionService;
+import ni.gob.inss.sisinv.model.entity.catalogo.Delegacion;
 
 @Named
 @Scope("view")
 public class DelegacionBackBean extends BaseBackBean implements Serializable  {
 
 	private static final long serialVersionUID = 1L;
+	
 	private String txtBusquedaDelegacionByNombre;
-	private String nombreDelegacion;
 	private boolean nuevoRegistro;
-	private Integer hfId;
 	private List<Catalogo> listaDepartamentos;
 	private TiposCatalogo catalogoDepartamento;
+	
+	private List<Delegacion> listaDelegaciones;
+	private Delegacion delegacionSeleccionada;
+	
+	private String nombreDelegacion;
+	private Integer hfId;
+	private boolean pasivo;
+	private Integer departamentoId;
+	
 	private final static int CATALOGO_DEPARTAMENTO = 1;
 	
 	@Autowired
@@ -35,9 +45,13 @@ public class DelegacionBackBean extends BaseBackBean implements Serializable  {
 	@Autowired
 	CatalogoService oCatalogoService;
 	
+	@Autowired
+	DelegacionService oDelegacionService;
+	
 	
 	@PostConstruct
 	public void init(){
+		this.limpiar();
 		this.cargarListaDepartamentos();
 	}
 	
@@ -62,8 +76,52 @@ public class DelegacionBackBean extends BaseBackBean implements Serializable  {
 	
 	}
 	
-	public void guardarNuevaDelegacion(){
-		
+	public void guardarOrActualizar(){
+		//TODO: POR EL MOMENTO SOLO TENEMOS EL METODO GUARDAR, ESTE METODO MANEJARA AMBOS GUARDAR Y ACTUALIZAR
+		this.guardar();
+	}
+	
+	public void limpiar(){
+		this.setTxtBusquedaDelegacionByNombre("");
+		this.setNuevoRegistro(true);
+		this.setHfId(null);
+		this.setNombreDelegacion("");
+		this.setDepartamentoId(null);
+		this.setPasivo(false);
+	}
+	
+	public void guardar(){
+		try {
+			Delegacion oDelegacion = new Delegacion();
+			//Por ser nueva Delegacion por defecto es activo.
+			oDelegacion.setPasivo(false); 
+			oDelegacion.setNombre(this.getNombreDelegacion());
+			oDelegacion.setDepartamentoId(this.getDepartamentoId());
+			oDelegacionService.agregar(oDelegacion);
+			mostrarMensajeInfo(MessagesResults.EXITO_GUARDAR);
+		} catch (Exception e) {
+            mostrarMensajeError(this.getClass().getSimpleName(),"guardarNuevaDelegacion",MessagesResults.ERROR_GUARDAR, e);
+
+		} 
+	}
+	
+	
+
+	public List<Delegacion> getListaDelegaciones() {
+		return listaDelegaciones;
+	}
+
+	public void setListaDelegaciones(List<Delegacion> listaDelegaciones) {
+		this.listaDelegaciones = listaDelegaciones;
+	}
+
+	
+	public Delegacion getDelegacionSeleccionada() {
+		return delegacionSeleccionada;
+	}
+
+	public void setDelegacionSeleccionada(Delegacion delegacionSeleccionada) {
+		this.delegacionSeleccionada = delegacionSeleccionada;
 	}
 
 	public String getNombreDelegacion() {
@@ -100,6 +158,23 @@ public class DelegacionBackBean extends BaseBackBean implements Serializable  {
 
 	public List<Catalogo> getListaDepartamentos() {
 		return listaDepartamentos;
-	}				
+	}
 
+	public boolean isPasivo() {
+		return pasivo;
+	}
+
+	public void setPasivo(boolean pasivo) {
+		this.pasivo = pasivo;
+	}
+
+	public Integer getDepartamentoId() {
+		return departamentoId;
+	}
+
+	public void setDepartamentoId(Integer departamentoId) {
+		this.departamentoId = departamentoId;
+	}				
+	
+	
 }

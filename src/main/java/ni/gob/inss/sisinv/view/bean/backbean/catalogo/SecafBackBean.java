52b4be38ad79e1753b2a5e5a69411140eb21299b
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-09-06 14:45:09 -0600

    Catalogo Secaf - Clase BackBean

---
+++ src/main/java/ni/gob/inss/sisinv/view/bean/backbean/catalogo/SecafBackBean.java
@@ -0,0 +1,22 @@
+package ni.gob.inss.sisinv.view.bean.backbean.catalogo;
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import ni.gob.inss.barista.view.bean.backbean.BaseBackBean;
+
+/**
+ * 
+ * @author fbriceno
+ *
+ */
+@Named
+@Scope("view")
+public class SecafBackBean extends BaseBackBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+}

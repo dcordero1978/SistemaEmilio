efdbf2f980fdaa87e70ebd23962bfac096ad1ccf
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-09-20 15:23:46 -0600

    Pantalla de Registro de Activos - Creación de Vista y BackBean.

---
+++ src/main/java/ni/gob/inss/sisinv/view/bean/backbean/inventario/RegistroActivosBackBean.java
@@ -0,0 +1,13 @@
+package ni.gob.inss.sisinv.view.bean.backbean.inventario;
+
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import ni.gob.inss.barista.view.bean.backbean.BaseBackBean;
+
+@Named
+@Scope("view")
+public class RegistroActivosBackBean extends BaseBackBean {
+	
+}

5236fbfd8a59a08b5f4bbe776cbc6fd6d79fc834
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-09-23 08:16:23 -0600

    Se agrega MarcaModeloDAO  y MarcaModeloDAOImpl

---
+++ src/main/java/ni/gob/inss/sisinv/model/dao/catalogos/MarcaModeloDAO.java
@@ -0,0 +1,8 @@
+package ni.gob.inss.sisinv.model.dao.catalogos;
+
+import ni.gob.inss.barista.model.dao.BaseGenericDAO;
+import ni.gob.inss.sisinv.model.entity.catalogo.MarcasModelos;
+
+public interface MarcaModeloDAO extends BaseGenericDAO<MarcasModelos, Integer>{
+
+}
---
+++ src/main/java/ni/gob/inss/sisinv/model/daoImpl/catalogos/MarcaModeloImpl.java
@@ -0,0 +1,9 @@
+package ni.gob.inss.sisinv.model.daoImpl.catalogos;
+
+import ni.gob.inss.barista.model.daoImpl.BaseGenericDAOImpl;
+import ni.gob.inss.sisinv.model.dao.catalogos.MarcaModeloDAO;
+import ni.gob.inss.sisinv.model.entity.catalogo.MarcasModelos;
+
+public class MarcaModeloImpl extends BaseGenericDAOImpl<MarcasModelos, Integer> implements MarcaModeloDAO {
+
+}

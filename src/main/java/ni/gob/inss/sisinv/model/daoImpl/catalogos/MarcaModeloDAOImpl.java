a5e1bfd94719c23bd1f12893149e6e1656780200
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-09-23 08:19:05 -0600

    Corrección en Nombre de Dao de MarcaModelo

--- src/main/java/ni/gob/inss/sisinv/model/daoImpl/catalogos/MarcaModeloDAOImpl.java
+++ src/main/java/ni/gob/inss/sisinv/model/daoImpl/catalogos/MarcaModeloDAOImpl.java
@@ -1,9 +1,12 @@
 package ni.gob.inss.sisinv.model.daoImpl.catalogos;
 
+import org.springframework.stereotype.Repository;
+
 import ni.gob.inss.barista.model.daoImpl.BaseGenericDAOImpl;
 import ni.gob.inss.sisinv.model.dao.catalogos.MarcaModeloDAO;
 import ni.gob.inss.sisinv.model.entity.catalogo.MarcasModelos;
 
-public class MarcaModeloImpl extends BaseGenericDAOImpl<MarcasModelos, Integer> implements MarcaModeloDAO {
+@Repository
+public class MarcaModeloDAOImpl extends BaseGenericDAOImpl<MarcasModelos, Integer> implements MarcaModeloDAO {
 
 }

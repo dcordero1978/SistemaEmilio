b6ee55ed623d5809891f2f9c0a8f10e1fd28d216
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-09-23 08:50:00 -0600

    Se agregan nuevos servicios que extienden de catalogo

---
+++ src/main/java/ni/gob/inss/sisinv/bussineslogic/service/catalogos/CatalogoExtService.java
@@ -0,0 +1,12 @@
+package ni.gob.inss.sisinv.bussineslogic.service.catalogos;
+
+import java.util.List;
+
+import ni.gob.inss.barista.businesslogic.service.catalogos.CatalogoService;
+import ni.gob.inss.barista.model.dao.EntityNotFoundException;
+import ni.gob.inss.sisinv.model.entity.catalogo.MarcasModelos;
+
+public interface CatalogoExtService extends CatalogoService {
+	public List<MarcasModelos> obtenerListaMarcas()throws EntityNotFoundException;
+	public List<MarcasModelos> obtenerListaModelos()throws EntityNotFoundException;
+}
---
+++ src/main/java/ni/gob/inss/sisinv/bussineslogic/serviceImpl/catalogos/CatalogoExtServiceImpl.java
@@ -0,0 +1,37 @@
+package ni.gob.inss.sisinv.bussineslogic.serviceImpl.catalogos;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.googlecode.genericdao.search.Filter;
+import com.googlecode.genericdao.search.Search;
+
+import ni.gob.inss.barista.businesslogic.serviceImpl.catalogos.CatalogoServiceImpl;
+import ni.gob.inss.barista.model.dao.EntityNotFoundException;
+import ni.gob.inss.sisinv.bussineslogic.service.catalogos.CatalogoExtService;
+import ni.gob.inss.sisinv.model.dao.catalogos.MarcaModeloDAO;
+import ni.gob.inss.sisinv.model.entity.catalogo.MarcasModelos;
+
+@Primary
+public class CatalogoExtServiceImpl extends CatalogoServiceImpl implements CatalogoExtService {
+	@Autowired
+	MarcaModeloDAO oMarcaModeloDAO;
+	
+	@Transactional
+	@Override
+	public List<MarcasModelos> obtenerListaMarcas() throws EntityNotFoundException {
+		Search oSearch = new Search();
+		oSearch.addFilter(Filter.equal("padreId", 0));
+		return oMarcaModeloDAO.search(oSearch);		
+	}
+
+	@Override
+	public List<MarcasModelos> obtenerListaModelos() throws EntityNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

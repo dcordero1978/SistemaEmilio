5a16dcba634964c1f17a6082b62a030ae5e87d29
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-11-23 10:23:02 -0600

    BackBean CaracteristicasHardware

---
+++ src/main/java/ni/gob/inss/sisinv/view/bean/backbean/catalogo/CaracteristicasHardwareBackBean.java
@@ -0,0 +1,21 @@
+package ni.gob.inss.sisinv.view.bean.backbean.catalogo;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import ni.gob.inss.barista.view.bean.backbean.BaseBackBean;
+
+@Named
+@Scope("view")
+public class CaracteristicasHardwareBackBean extends BaseBackBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@PostConstruct
+	public void init(){}
+
+}

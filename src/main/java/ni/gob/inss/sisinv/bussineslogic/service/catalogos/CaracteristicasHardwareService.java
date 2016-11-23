2825441e19cce0ae8b21b0bf43bb9b1e8979f954
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-11-23 10:01:11 -0600

    Componentes Caracteristicas Hardware - Servicio y DAO. Se agregan los componentes necesarios para el desarrollo de la pantalla. La entidad CaracteristicasHardware El servicio y su DAO

---
+++ src/main/java/ni/gob/inss/sisinv/bussineslogic/service/catalogos/CaracteristicasHardwareService.java
@@ -0,0 +1,5 @@
+package ni.gob.inss.sisinv.bussineslogic.service.catalogos;
+
+public interface CaracteristicasHardwareService {
+
+}
---
+++ src/main/java/ni/gob/inss/sisinv/bussineslogic/serviceImpl/catalogos/CaracteristicasHardwareServiceImpl.java
@@ -0,0 +1,10 @@
+package ni.gob.inss.sisinv.bussineslogic.serviceImpl.catalogos;
+
+import org.springframework.stereotype.Service;
+
+import ni.gob.inss.sisinv.bussineslogic.service.catalogos.CaracteristicasHardwareService;
+
+@Service
+public class CaracteristicasHardwareServiceImpl implements CaracteristicasHardwareService{
+	
+}

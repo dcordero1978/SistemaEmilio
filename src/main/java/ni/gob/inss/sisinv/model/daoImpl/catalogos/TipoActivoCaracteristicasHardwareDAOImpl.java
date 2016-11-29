030bc5c2ca8d74ce6da205c14de466754aafa4ec
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2016-11-29 09:13:22 -0600

    Capa de acceso a Datos de TipoActivoCaracteristicasHardware.

---
+++ src/main/java/ni/gob/inss/sisinv/model/daoImpl/catalogos/TipoActivoCaracteristicasHardwareDAOImpl.java
@@ -0,0 +1,9 @@
+package ni.gob.inss.sisinv.model.daoImpl.catalogos;
+
+import ni.gob.inss.barista.model.daoImpl.BaseGenericDAOImpl;
+import ni.gob.inss.sisinv.model.dao.catalogos.TipoActivoCaracteristicasHardwareDAO;
+import ni.gob.inss.sisinv.model.entity.catalogo.TipoActivoCaracteristicasHardware;
+
+public class TipoActivoCaracteristicasHardwareDAOImpl extends BaseGenericDAOImpl<TipoActivoCaracteristicasHardware, Integer>
+		implements TipoActivoCaracteristicasHardwareDAO {
+}

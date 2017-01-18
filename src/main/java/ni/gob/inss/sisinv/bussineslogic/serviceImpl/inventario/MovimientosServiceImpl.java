afa30f10876a6277c3e1b2df6ba0c912cb28676e
Francisco Briceño <fbriceno@economiafamiliar.gob.ni>
2017-01-18 10:39:47 -0600

    Mejora en excepciones de metodos guardar y buscar MovimientoService Se elimina la excepcion del javax.persistence.EntityNotFoundException Se implemente el barista.model.dao.EntityNotFoundException

--- src/main/java/ni/gob/inss/sisinv/bussineslogic/serviceImpl/inventario/MovimientosServiceImpl.java
+++ src/main/java/ni/gob/inss/sisinv/bussineslogic/serviceImpl/inventario/MovimientosServiceImpl.java
@@ -2,16 +2,13 @@ package ni.gob.inss.sisinv.bussineslogic.serviceImpl.inventario;
 
 import java.util.List;
 
-import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.googlecode.genericdao.search.Filter;
-import com.googlecode.genericdao.search.Search;
-
 import ni.gob.inss.barista.model.dao.DAOException;
+import ni.gob.inss.barista.model.dao.EntityNotFoundException;
 import ni.gob.inss.sisinv.bussineslogic.service.inventario.MovimientosService;
 import ni.gob.inss.sisinv.model.dao.inventario.MovimientosDAO;
 import ni.gob.inss.sisinv.model.entity.inventario.Movimientos;
@@ -25,13 +22,13 @@ public class MovimientosServiceImpl implements MovimientosService {
 	
 	@Transactional
 	@Override
-	public List<Movimientos> buscar(Integer activoId) throws EntityNotFoundException, ni.gob.inss.barista.model.dao.EntityNotFoundException {
+	public List<Movimientos> buscar(Integer activoId) throws EntityNotFoundException {
 		return oMovimientosDAO.buscar(activoId);
 	}
 
 	@Transactional
 	@Override
-	public void guardar(Movimientos oMovimientos) throws javax.persistence.EntityNotFoundException, DAOException {
+	public void guardar(Movimientos oMovimientos) throws DAOException{
 		oMovimientosDAO.saveUpper(oMovimientos);
 		
 	}
